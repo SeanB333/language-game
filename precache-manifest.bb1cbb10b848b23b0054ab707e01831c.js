016dc7f9b40c4db5a92129428fccce17a31e112d
Sean Brennan <spb4578@gmail.com>
2019-01-14 14:02:29 -0600

    Updates

---
+++ precache-manifest.bb1cbb10b848b23b0054ab707e01831c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "daf6f83ab1b174b5b47e",
+    "url": "/seanB333.github.io/language-game/static/js/runtime~main.daf6f83a.js"
+  },
+  {
+    "revision": "d35c06262fd51555265e",
+    "url": "/seanB333.github.io/language-game/static/js/main.d35c0626.chunk.js"
+  },
+  {
+    "revision": "69270dab7128313bfabb",
+    "url": "/seanB333.github.io/language-game/static/js/1.69270dab.chunk.js"
+  },
+  {
+    "revision": "d35c06262fd51555265e",
+    "url": "/seanB333.github.io/language-game/static/css/main.5b52dfb4.chunk.css"
+  },
+  {
+    "revision": "69270dab7128313bfabb",
+    "url": "/seanB333.github.io/language-game/static/css/1.ec4d2b72.chunk.css"
+  },
+  {
+    "revision": "409a362ea1ddd42acf2709c3d4c1a85c",
+    "url": "/seanB333.github.io/language-game/index.html"
+  }
+];
